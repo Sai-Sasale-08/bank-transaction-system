0e368596b6fc7c393e2f4c6d475e00fe70f667ff
7778862668 <5114saisasale@gmail.com>
2026-07-21 00:16:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieparser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Bank Transaction System API");
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/account", accountRouter);
 app.use("/api/transaction", transactionRouter);
